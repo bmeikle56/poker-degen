e322b8f129c1354b35a105fc6d9de2ba6fa8a508
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2025-07-07 20:13:42 -0400

    Scaffold commit

---
+++ PokerDegen/PokerDegen/PokerDegenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerDegenApp.swift
+//  PokerDegen
+//
+//  Created by Braeden Meikle on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PokerDegenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokerDegen/PokerDegenTests/PokerDegenTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokerDegenTests.swift
+//  PokerDegenTests
+//
+//  Created by Braeden Meikle on 7/7/25.
+//
+
+import Testing
+@testable import PokerDegen
+
+struct PokerDegenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
