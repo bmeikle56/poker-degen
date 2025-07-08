4f9997149169e54863bf60c647f43467154c9594
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2025-07-07 23:16:59 -0400

    Add hidden hand

--- PokerDegen/PokerDegen/Shapes/Shapes.swift
+++ PokerDegen/PokerDegen/Shapes/Shapes.swift
@@ -23,34 +23,3 @@ struct DiamondShape: Shape {
     }
 }
 
-struct PokerTable: View {
-    var body: some View {
-        GeometryReader { geo in
-            let scale: CGFloat = 0.75
-            let width = geo.size.width * scale
-            let height = geo.size.height * scale
-
-            ZStack {
-                Capsule()
-                    .fill(Color.green.opacity(0.3))
-                    .rotation3DEffect(
-                            .degrees(40),
-                            axis: (x: 1, y: 0, z: 0),
-                            perspective: 0.5
-                        )
-                Capsule()
-                    .stroke(Color.pokerMaroon, lineWidth: 6)
-                    .rotation3DEffect(
-                            .degrees(40),
-                            axis: (x: 1, y: 0, z: 0),
-                            perspective: 0.5
-                        )
-            }
-            .frame(width: width, height: height)
-            .position(x: geo.size.width / 2, y: geo.size.height / 2)
-            .shadow(radius: 10)
-        }
-        .aspectRatio(0.6, contentMode: .fit) // Vertical layout
-        .padding()
-    }
-}
--- PokerDegen/PokerDegen/Views/Main/MainView.swift
+++ PokerDegen/PokerDegen/Views/Main/MainView.swift
@@ -7,12 +7,61 @@
 
 import SwiftUI
 
+struct PokerTable: View {
+    var body: some View {
+        GeometryReader { geo in
+            let scale: CGFloat = 0.75
+            let width = geo.size.width * scale
+            let height = geo.size.height * scale
+
+            ZStack {
+                Capsule()
+                    .fill(Color.green.opacity(0.3))
+                    .rotation3DEffect(
+                            .degrees(45),
+                            axis: (x: 1, y: 0, z: 0),
+                            perspective: 0.5
+                        )
+                Capsule()
+                    .stroke(Color.pokerMaroon, lineWidth: 6)
+                    .rotation3DEffect(
+                            .degrees(45),
+                            axis: (x: 1, y: 0, z: 0),
+                            perspective: 0.5
+                        )
+            }
+            .frame(width: width, height: height)
+            .position(x: geo.size.width / 2, y: geo.size.height / 2)
+            .shadow(radius: 10)
+        }
+        .aspectRatio(0.6, contentMode: .fit)
+        .padding()
+    }
+}
+
+
 struct MainView: View {
     let navigationController: UINavigationController
 
     var body: some View {
         VStack {
             PokerTable()
+            HStack(spacing: -38.0) {
+                Button(action: {}) {
+                    Image("card")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 80, height: 80)
+                        .rotationEffect(.degrees(-6))
+                }
+                Button(action: {}) {
+                    Image("card")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 80, height: 80)
+                        .rotationEffect(.degrees(6))
+                }
+            }
         }
         .navigationBarHidden(true)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
