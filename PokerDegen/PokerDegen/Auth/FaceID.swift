5d6cc2e7137ed67dc0bfe0d6fbe78a5728e6740f
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2025-07-07 21:05:14 -0400

    Add faceID boilerplate

---
+++ PokerDegen/PokerDegen/Auth/FaceID.swift
@@ -0,0 +1,21 @@
+//
+//  FaceID.swift
+//  PokerDegen
+//
+//  Created by Braeden Meikle on 7/7/25.
+//
+
+import LocalAuthentication
+
+func authenticateWithFaceID() async throws -> Bool {
+    let context = LAContext()
+    let reason = "Unlock with Face ID"
+
+    var error: NSError?
+    guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+        throw error ?? NSError(domain: "FaceID", code: -1, userInfo: [NSLocalizedDescriptionKey: "Biometric authentication not available"])
+    }
+
+    let success = try await context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason)
+    return success
+}
